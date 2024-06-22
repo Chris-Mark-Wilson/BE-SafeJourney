ef3ece7eea7b0ccba1931a968785f1153ec8f6d7
Chris-Mark-Wilson <chris@cmwilson.co.uk>
2024-06-22 10:45:25 +0100

    changed to port 3000, .env.development - render fail to deploy test

--- listen.js
+++ listen.js
@@ -1,5 +1,5 @@
 const app = require('./app.js')
-require('dotenv').config({path:`${__dirname}/.env`})
+require('dotenv').config({path:`${__dirname}/.env.development`})
 const { PORT = 9090 } = process.env
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`))
